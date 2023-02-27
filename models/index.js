53215075046491466c3298788290eec9700587a0
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2023-02-27 21:38:05 +0300

    changed dialect to postgress

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("ecomerce", "root", "", {
-  host: "localhost",
-  dialect: "mysql", 
-  logging: false,
-});
+require('dotenv').config()
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host:process.env.HOST,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 const db = {};
 
@@ -13,17 +18,17 @@ db.sequelize = sequelize;
 
 db.Product = require("./Product.js")(sequelize, Sequelize);
 db.Product_Images = require("./Product_Images.js")(sequelize, Sequelize);
-db.Categories = require('./Category.js')(sequelize,Sequelize)
-db.Reviews = require('./Review.js')(sequelize,Sequelize)
+db.Categories = require("./Category.js")(sequelize, Sequelize);
+db.Reviews = require("./Review.js")(sequelize, Sequelize);
 db.Product.hasMany(db.Product_Images, {
   onDelete: "cascade",
 });
-db.Product_Images.belongsTo(db.Product)
-db.Product.hasMany(db.Reviews,{
-    onDelete:"cascade"
-})
-db.Reviews.belongsTo(db.Product)
-db.Categories.hasMany(db.Product)
-db.Product.belongsTo(db.Categories)
+db.Product_Images.belongsTo(db.Product);
+db.Product.hasMany(db.Reviews, {
+  onDelete: "cascade",
+});
+db.Reviews.belongsTo(db.Product);
+db.Categories.hasMany(db.Product);
+db.Product.belongsTo(db.Categories);
 
 module.exports = db;
