c3b88147f8b7564ff371b1604ffa3df1018441be
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2023-03-25 08:58:13 +0300

    made changes

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const jwt = require('jsonwebtoken')
 exports.generateToken = (id) => {
+  console.log(process.env.refresh_token_secret,process.env.access_token_secret);
   const access_token = jwt.sign({ id }, process.env.access_token_secret, {
     expiresIn: "1m",
   });
