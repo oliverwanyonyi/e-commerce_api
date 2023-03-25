0559e29a7ccdd8f6e0df91d7ca866bd0e71bc1f3
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2023-03-25 11:17:47 +0300

    made changes

--- server.js
+++ server.js
@@ -20,22 +20,34 @@ app.use(cookieParser());
 
 // set up cors
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    preflightContinue: true,
-    maxAge: 3600,
-    // Add Access-Control-Allow-Origin header here
-    exposedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Origin",
-    ],
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin','https;//shopyetu.netlify.app');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     preflightContinue: true,
+//     maxAge: 3600,
+//     // Add Access-Control-Allow-Origin header here
+//     exposedHeaders: [
+//       "Content-Type",
+//       "Authorization",
+//       "Access-Control-Allow-Origin",
+//     ],
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
