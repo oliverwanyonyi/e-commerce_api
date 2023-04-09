4a3388399e88cac46e121d4b57bf6ce976418ad5
oliverwanyonyi <oliverwanyonyi02@gmail.com>
2023-04-09 14:32:14 +0300

    error fix

--- server.js
+++ server.js
@@ -26,8 +26,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-  //  origin:["http://localhost:3000", "https://shopyetu.netlify.app"],
-  origin:"*",
+   origin:["http://localhost:3000", "https://shopyetu.netlify.app"],
     credentials: true,
   })
 )
